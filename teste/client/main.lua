ce6e4210e8e1f983388feccc66b4cc4b0305768c
Ruben Bernardo <ruben.bernardo@outlook.com>
2020-07-30 13:54:06 +0100

    a
    a

--- teste/client/main.lua
+++ teste/client/main.lua
@@ -13,21 +13,12 @@ Citizen.CreateThread(function()
     PlayerData = ESX.GetPlayerData()
 end)
 
-
-RegisterCommand("dinheiro", function(source, args, rawCommand)
-    ESX.TriggerServerCallback("teste:money", function(dinheiro)
-        ESX.ShowNotification("Voce tem: ~g~"..dinheiro.."$ ~w~ na sua carteira.")
-    end)
-end)
-
 RegisterCommand("id", function(source, raw, args)
 	ESX.ShowNotification("ID: ~g~" .. GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1))))
 end, false)
 
-RegisterCommand("help", function())
-    TriggerEvent('chat:addMessage', {
-        color = {255,0,0},
-        multiline = true,
-        args = {'[SERVER]', 'Scripting 4life'} --prefix and message
-    })
+RegisterCommand("coo", function()
+    print(GetEntityCoords(PlayerPedId()))
+end)
+
 
---
+++ teste/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+
+games 'gta5'
+
+author 'deza'
+description 'misc commands'
+version '1.0.0'
+
+client_scripts {'config.lua',
+    'client/main.lua'
+}
+
+server_scripts {'config.lua',
+    'server/main.lua'
+}
