1a7ebf75a53af8b60b31dfcb9852295632d00644
Ruben Bernardo <ruben.bernardo@outlook.com>
2020-07-30 03:55:47 +0100

    typo

--- teste/server/main.lua
+++ teste/server/main.lua
@@ -2,7 +2,7 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterServerCallback("teste:money", function(source,db)
+ESX.RegisterServerCallback("teste:money", function(source,cb)
     local xPlayer = ESX.GetPlayerFromId(source)
     local pdinheiro = xPlayer.getMoney()
 
