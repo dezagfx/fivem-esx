17d1a0f7e0ee26de581405c8b77e3d8f626c2130
Ruben Bernardo <ruben.bernardo@outlook.com>
2020-07-30 14:30:15 +0100

    Update main.lua

--- teste/client/main.lua
+++ teste/client/main.lua
@@ -1,24 +1,79 @@
-ESX = nil
+ESX              = nil
+local PlayerData = {}
 
 Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
-    end
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
 
-    while ESX.GetPlayerData().job == nil do
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+  PlayerData = xPlayer   
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+  PlayerData.job = job
+end)
+
+--------------------------------
+local isMenuOpen = false
+local options = {
+    {label = "Spawn Carbine afenx -.-", value = 'spawn_carbine'}, --spawn carabine rifle
+    {label = "Suicide", value = 'suicide'}, --commit suicide
+    {label = 'id', value = 'id'} --show self id
+}
+
+function OpenMenu()
+    isMenuOpen = true
+    ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'rp_menu',{
+        title    = "RP Menu",
+        align    = "rigth",
+        elements = options
+    }, function(data, menu)
+        menu.close()
+        isMenuOpen = false
+        if data.current.value == 'spawn_carbine' then
+            SpawnCarbine()
+        end
+        if data.current.value == 'suicide' then
+            Suicide()
+        end
+        if data.current.value == 'id' then
+            ShowId()
+        end
+    end,
+    function(data, menu)
+        menu.close()
+        isMenuOpen = false
+    end)
+end
+
+function SpawnCarbine()
+    GiveWeaponToPed(PlayerPedId(), GetHashKey("WEAPON_CARBINERIFLE"), 300, 0, 1)
+    ESX.ShowNotification('Recebeste uma carabina enviada pelos deuses')
+end
+
+function Suicide()
+    ESX.ShowNotification("WASTED")
+    SetEntityHealth(PlayerPedId(), 0)
+end
+
+function ShowId()
+    ESX.ShowNotification(PlayerId())
+end
+
+Citizen.CreateThread(function()
+    while true do
+        if isControllJustReleased(0,47) and not isMenuOpen then
+            OpenMenu()
+        end
         Citizen.Wait(10)
     end
 
-    PlayerData = ESX.GetPlayerData()
 end)
 
-RegisterCommand("id", function(source, raw, args)
-	ESX.ShowNotification("ID: ~g~" .. GetPlayerServerId(NetworkGetEntityOwner(GetPlayerPed(-1))))
-end, false)
-
-RegisterCommand("coo", function()
-    print(GetEntityCoords(PlayerPedId()))
-end)
 
 
